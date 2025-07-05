c9ed6250403c84de1a185abe17005d77d0e07917
ibsa-a1 <ibsaabera712@gmail.com>
2025-07-05 21:43:20 +0300

    Finish our Setup

---
+++ to-do-list-app/src/Components/Todolist.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Todolist() {
+  return (
+    <div>
+        <div className="todo-container">
+            <h1>To-Do List</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Todolist
