5aa798c2c9f067abfec5d0e80f0afb097e723bfc
ibsa-a1 <ibsaabera712@gmail.com>
2025-07-05 22:12:58 +0300

    Add buttons

--- to-do-list-app/src/Components/Todolist.jsx
+++ to-do-list-app/src/Components/Todolist.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Todolist.css'
+import {BiCheckDouble, BiEdit, BiTrash, BiCheckCircle, BiReset, BiRefresh} from 'react-icons/bi'
 
 function Todolist() {
   return (
@@ -8,6 +9,11 @@ function Todolist() {
             <h1>To-Do List</h1>
             <div className="input-section">
                 <input type="text" className="input-field" />
+                <>
+                <button className="update-btn"><BiCheckDouble /></button>
+                <button className="cancel-btn"><BiRefresh /></button>
+                <button className="add-btn">Add</button>
+                </>
             </div>
         </div>
     </div>
