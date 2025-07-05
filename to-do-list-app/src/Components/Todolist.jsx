eb18fb8ce65d8d62383192c6f16b8b78d377342f
ibsa-a1 <ibsaabera712@gmail.com>
2025-07-05 22:02:31 +0300

    basic structure and style updated

--- to-do-list-app/src/Components/Todolist.jsx
+++ to-do-list-app/src/Components/Todolist.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import './Todolist.css'
 
 function Todolist() {
   return (
     <div>
         <div className="todo-container">
             <h1>To-Do List</h1>
+            <div className="input-section">
+                <input type="text" className="input-field" />
+            </div>
         </div>
     </div>
   )
